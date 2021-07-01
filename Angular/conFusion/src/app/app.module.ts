4f5f86bf562609ac1fa62dac84f6f6dbe1c34083
SteppenHasen <dustasura@gmail.com>
2021-07-01 17:23:32 +0300

    Components Part 1

--- Angular/conFusion/src/app/app.module.ts
+++ Angular/conFusion/src/app/app.module.ts
@@ -6,18 +6,22 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
+import { MatListModule } from '@angular/material/list';
 
 import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
