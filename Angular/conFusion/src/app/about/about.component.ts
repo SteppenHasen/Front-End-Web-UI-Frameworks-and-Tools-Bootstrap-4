82b62b57792b4b926fc2ce5220ae477ae2436746
SteppenHasen <dustasura@gmail.com>
2021-07-11 03:54:11 +0300

    AboutUs Component and Routing

--- Angular/conFusion/src/app/about/about.component.ts
+++ Angular/conFusion/src/app/about/about.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,  } from '@angular/core';
+import { Params, ActivatedRoute } from '@angular/router';
+import { Leader } from '../shared/leader';
+import { LeaderService } from '../services/leader.service';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  leaders: Leader[];
+
+  constructor(private leaderservice: LeaderService,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.leaders = this.leaderservice.getLeaders();
   }
 
 }
